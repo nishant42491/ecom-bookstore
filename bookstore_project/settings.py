dbda4ce8e8e0f79aa4b161a972d761fd29816260
nishant <n.rajadhyaksha@somaiya.edu>
2021-04-16 14:29:50 +0530

    ch12

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -138,6 +138,8 @@ STATICFILES_FINDERS = [
 "django.contrib.staticfiles.finders.FileSystemFinder",
 "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
+MEDIA_URL = '/media/' # new
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
 AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
