3022f40cc2c1b9e41eb8add67e7e563173600f7e
nishant <n.rajadhyaksha@somaiya.edu>
2021-04-16 13:33:41 +0530

    ch8

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')), # new
     path('',include('pages.urls')),
+    path('books/', include('books.urls')),
     
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
+from django.views.generic import TemplateView
+
 # Create your views here.
 
-from django.views.generic import TemplateView
+
+
+
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+    
 
 class AboutPageView(TemplateView): # new
     template_name = 'about.html'
